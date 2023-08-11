18f29f8d508bb756b375e6adf0a702e2dd008f92
t-rakuyama <t.rakuyama929@gmail.com>
2023-08-11 17:12:43 +0900

    feat: チュートリアル実施

--- lib/cdk-hello-world-stack.ts
+++ lib/cdk-hello-world-stack.ts
@@ -1,16 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
-export class CdkHelloWorldStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+import { CdkHelloWorld } from './cdk-hello-world';
+  
+export class CdkHelloWorldStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkHelloWorldQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CdkHelloWorld(this, 'hello-world');
   }
 }
